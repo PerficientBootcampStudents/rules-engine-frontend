9b05c02633fbe263e8c0ccc8b9bd82accda1f26d
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2022-12-12 15:55:18 -0500

    filetype modified to .ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,20 @@
-import * as axios from 'axios';
+import axios from 'axios';
 
 export default class Api {
+
+  private api: any;
+
   constructor() {
     this.api = axios.create({
       baseURL: 'http://localhost:8080/',
     });
   }
 
-  get(path) {
+  get(path: String) {
     return this.api.get(path);
   }
 
-  post(path, data) {
+  post(path: String, data: String) {
     return this.api.post(path, data);
   }
 }
