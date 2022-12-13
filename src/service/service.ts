4e4ca6d7dace1fea7c6e5ecf3ff628ccee708c9d
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2022-12-12 20:19:53 -0500

    service implementation craeted for data management

---
+++ src/service/service.ts
@@ -0,0 +1,25 @@
+import Api from '../api/api';
+import Checker from '../validation/checker';
+import Converter from './converter';
+
+export default class Service {
+
+    private api: Api;
+    private checker: Checker;
+    private converter: Converter;
+
+    constructor() {
+        this.api = new Api();
+    }
+
+    applyRule(rule: String) {
+        try{
+            this.checker.validate(rule);
+            var data: String = this.converter.toBody(rule)
+            this.api.post('/', data);   // TODO - complete path when Back API is ready
+
+        } catch (bse: BadSyntaxException) {
+            throw bse;
+        }
+    }
+}
