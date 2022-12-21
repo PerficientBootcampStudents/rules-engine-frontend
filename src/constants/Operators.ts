4b031270a5caba37a4b83bab65fb0afec09a1b42
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2022-12-20 20:36:19 -0500

    operators contants added

---
+++ src/constants/Operators.ts
@@ -0,0 +1,9 @@
+export enum Operators {
+    AND = " y ",
+    OR = " o ",
+    LESS = "es menor a",
+    GREATER = "es mayor a",
+    EQUAL = "es igual a",
+    DIFFERENT = "es diferente a",
+    IS = "es",
+}
