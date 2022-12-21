86c8ee7d167bb6b121947193ae92c77bdcdbe756
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2022-12-20 20:33:47 -0500

    error codes modified for better understanding

--- src/error/BadSyntaxError.ts
+++ src/error/BadSyntaxError.ts
@@ -6,7 +6,6 @@ export default class BadSyntaxError extends Error {
     public static readonly CODE_02 = new BadSyntaxError(BadSyntaxErrorCode.CODE_02, 'CODE_02');
     public static readonly CODE_03 = new BadSyntaxError(BadSyntaxErrorCode.CODE_03, 'CODE_03');
     public static readonly CODE_04 = new BadSyntaxError(BadSyntaxErrorCode.CODE_04, 'CODE_04');
-    public static readonly CODE_05 = new BadSyntaxError(BadSyntaxErrorCode.CODE_05, 'CODE_05');
 
     public constructor(readonly message: string, private readonly code: string) {
         super(message);
